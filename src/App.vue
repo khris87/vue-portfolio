92e9c2fbfb31f3baba416d27cfce7357f2124e59
khris87 <45290556+khris87@users.noreply.github.com>
2019-05-23 22:00:29 +0200

    vue-cli portfolio
    premier commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img @click="show = !show" id="vue-logo" alt="Vue logo" src="./assets/logo.png">
+    <transition name="bounce">
+    <About
+      v-if="show"
+      title="portfolio"
+      firstname="christophe"
+      lastname="masdoumier"
+    />
+    </transition>
+    <footer>
+      <HelloWorld msg="Portfolio Vue.js App"/>
+    </footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import About from './components/About.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    About
+  },
+  data () {
+    return {
+      show: false
+    }
   }
 }
 </script>
@@ -21,8 +38,28 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
+}
+#vue-logo {
+  width: 100px;
+  cursor: pointer;
+}
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h3>{{ firstname }}</h3>
+    <h3>{{ lastname }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    title: String,
+    firstname: String,
+    lastname: String
+  }
+}
+</script>
